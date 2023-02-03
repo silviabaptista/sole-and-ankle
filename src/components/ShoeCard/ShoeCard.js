51b6d3c3274f2b660763c02ddfaa1e6150bd8dc5
Silvia Lopez <silvia.lopez@nuance.com>
2023-02-03 18:07:39 -0500

    solution for 4b

--- src/components/ShoeCard/ShoeCard.js
+++ src/components/ShoeCard/ShoeCard.js
@@ -35,15 +35,35 @@ const ShoeCard = ({
         <Link href={`/shoe/${slug}`}>
             <Wrapper>
                 <ImageWrapper>
+                    {variant === "on-sale" && <SaleFlag>Sale</SaleFlag>}
+                    {variant === "new-release" && (
+                        <NewFlag>Just Release!</NewFlag>
+                    )}
                     <Image alt="" src={imageSrc} />
                 </ImageWrapper>
                 <Spacer size={12} />
                 <Row>
                     <Name>{name}</Name>
-                    <Price>{formatPrice(price)}</Price>
+                    <Price
+                        style={{
+                            "--color":
+                                variant === "on-sale"
+                                    ? COLORS.gray[700]
+                                    : undefined,
+                            "--text-decoration":
+                                variant === "on-sale"
+                                    ? "line-through"
+                                    : undefined
+                        }}
+                    >
+                        {formatPrice(price)}
+                    </Price>
                 </Row>
                 <Row>
                     <ColorInfo>{pluralize("Color", numOfColors)}</ColorInfo>
+                    {variant === "on-sale" && (
+                        <SalePrice>{formatPrice(salePrice)}</SalePrice>
+                    )}
                 </Row>
             </Wrapper>
         </Link>
@@ -68,6 +88,8 @@ const Image = styled.img`
 
 const Row = styled.div`
     font-size: 1rem;
+    display: flex;
+    justify-content: space-between;
 `;
 
 const Name = styled.h3`
@@ -75,7 +97,10 @@ const Name = styled.h3`
     color: ${COLORS.gray[900]};
 `;
 
-const Price = styled.span``;
+const Price = styled.span`
+    color: var(--color);
+    text-decoration: var(--text-decoration);
+`;
 
 const ColorInfo = styled.p`
     color: ${COLORS.gray[700]};
@@ -84,6 +109,28 @@ const ColorInfo = styled.p`
 const SalePrice = styled.span`
     font-weight: ${WEIGHTS.medium};
     color: ${COLORS.primary};
+`;
+
+const Flag = styled.div`
+    position: absolute;
+    top: 12px;
+    right: -4px;
+    background: red;
+    height: 32px;
+    padding: 0 10px;
+    font-size: ${14 / 18}rem;
+    font-weight: ${WEIGHTS.bold};
+    line-height: 32px; //center text
+    color: ${COLORS.white};
+    border-radius: 2px;
+`;
+
+const SaleFlag = styled(Flag)`
+    background-color: ${COLORS.primary};
+`;
+
+const NewFlag = styled(Flag)`
+    background-color: ${COLORS.secondary};
 `;
 
 export default ShoeCard;
